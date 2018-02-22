ca66cd150954d9db139aa58b71140d88898e4638
varma-4 <varmadatla444@gmail.com>
2018-02-22 16:18:50 +0530

    Refactored DiscView by modularising it, and added animations when disc Enlarges and comes back to Normal Position.

--- PartyTime/DiscView.swift
+++ PartyTime/DiscView.swift
@@ -18,33 +18,130 @@ class DiscView: UIView {
         return frame
     }()
     
-    func smallDiscPath() -> (inner: UIBezierPath, outer: UIBezierPath) {
+    lazy var centerOfDisc: CGPoint = {
+        let center = CGPoint(x: 0, y: bounds.height / 2)
+        return center
+    }()
+
+    var enlargedDiscFrame: CGRect = {
+        let frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 10, height: UIScreen.main.bounds.height)
+        return frame
+    }()
+    
+    // MARK: - View Life Cycle Methods
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        if smallDiscModeOn {
+            
+            // Small Disc Outer semicircle
+            let smallDiscOuterShapeLayer = getCAShapeLayer(forPath: smallDiscPath().outer.path, fillColor: UIColor.partyTimeBlue())
+            // Small Disc Inner Semicircle
+            let smallDiscInnerShapeLayer = getCAShapeLayer(forPath: smallDiscPath().inner.path, fillColor: .white)
+            
+            if fromEnlargedState {
+                
+                let outerRangeCount = Int(enlargedDiscPath().outer.radius - smallDiscPath().outer.radius)
+                let innerRangeCount = Int(enlargedDiscPath().inner.radius - smallDiscPath().inner.radius + 20)
+                print(innerRangeCount)
+                // Get AnimationsGroup for enlargedDisc outer SemiCircle
+                let outerAnimsGroup = makeAnimationsGroupForEnlargedToSmall(withRange: outerRangeCount, intialRadius: enlargedDiscPath().outer.radius)
+    
+                // Get path for smallDisc Outer path
+                let animShapeLayer = getCAShapeLayer(forPath: smallDiscPath().outer.path, fillColor: UIColor.partyTimeBlue())
+                // Animate Enlarged to Small Disc
+                let animationGroupEnlargeToSmall = getAnimationGroup(withLowerLimit: 0, upperLimit: 0, animsGroup: outerAnimsGroup)
+                
+                // Add animationGroup to Outer Semicircle
+                animShapeLayer.add(animationGroupEnlargeToSmall, forKey: nil)
+                
+                // Get AnimationsGroup for enlargedDisc inner semicircle
+                let innerAnimsGroup = makeAnimationsGroupForEnlargedToSmall(withRange: innerRangeCount, intialRadius: enlargedDiscPath().inner.radius + 20)
+                
+                // Get path for smallDisc Inner path
+                let innerAnimShapeLayer = getCAShapeLayer(forPath: smallDiscPath().inner.path, fillColor: UIColor.white)
+                // Animate Enlarged to Small Disc Inner
+                let animationGroupEnlargeToSmallInner = getAnimationGroup(withLowerLimit: 0, upperLimit: 0, animsGroup: innerAnimsGroup)
+                
+                // Add animationGroup to Inner Semicircle
+                innerAnimShapeLayer.add(animationGroupEnlargeToSmallInner, forKey: nil)
+                
+                // Paint all the shapeLayers
+                addLayer(shapeLayer: smallDiscOuterShapeLayer)
+                addLayer(shapeLayer: animShapeLayer)
+                addLayer(shapeLayer: innerAnimShapeLayer)
+                
+                fromEnlargedState = false
+            } else {
+                // Animate Outer Semicircle
+                let smallDiscAnimGroup = getAnimationGroup(withLowerLimit: 0, upperLimit: Int(smallDiscPath().outer.radius))
+
+                // Add animationGroup to Outer semicircle Layer
+                smallDiscOuterShapeLayer.add(smallDiscAnimGroup, forKey: nil)
+
+                // First paint the Outer semicircle, followed by inner semicircle
+                addLayer(shapeLayer: smallDiscOuterShapeLayer)
+                addLayer(shapeLayer: smallDiscInnerShapeLayer)
+            }
+        } else {
+            layer.sublayers = nil
+            
+            // Outer Semicirle
+            let enlargedDiscOuterShapeLayer = getCAShapeLayer(forPath: enlargedDiscPath().outer.path, fillColor: UIColor.partyTimeBlue())
+            
+            // Animate Outer Semicircle
+            let enlargedOuterAnimationGroup = getAnimationGroup(withLowerLimit: 0, upperLimit: Int(enlargedDiscPath().outer.radius))
+            // Add Animation Object to Outer Semicircle
+            enlargedDiscOuterShapeLayer.add(enlargedOuterAnimationGroup, forKey: nil)
+            
+            // Inner SemiCircle
+            let enlargedDiscInnerShapeLayer = getCAShapeLayer(forPath: enlargedDiscPath().inner.path, fillColor: .white)
+            // Animate Inner Semicircle
+            let enlargedInnerAnimationsGroup = getAnimationGroup(withLowerLimit: 0, upperLimit: Int(enlargedDiscPath().inner.radius))
+            // Add Animation Group to shapeLayer
+            enlargedDiscInnerShapeLayer.add(enlargedInnerAnimationsGroup, forKey: nil)
+            
+            // First paint the Outer semicircle, followed by inner semicircle
+            addLayer(shapeLayer: enlargedDiscOuterShapeLayer)
+            addLayer(shapeLayer: enlargedDiscInnerShapeLayer)
+            fromEnlargedState = true
+        }
+    }
+    
+    func smallDiscPath() -> (inner: (path: UIBezierPath, radius: CGFloat), outer: (path: UIBezierPath, radius: CGFloat)) {
         let innerPath = getInnerSemiCircleBeizerpath(withRadius: 100)
         let outerPath = getOuterSemiCircleBeizerPath(withRadius: 100)
         return (innerPath, outerPath)
     }
     
-    func enlargedDiscPath() -> (inner: UIBezierPath, outer: UIBezierPath) {
+    func enlargedDiscPath() -> (inner: (path: UIBezierPath, radius: CGFloat), outer: (path: UIBezierPath, radius: CGFloat)) {
         let innerPath = getInnerSemiCircleBeizerpath(withRadius: 200)
         let outerPath = getOuterSemiCircleBeizerPath(withRadius: 200)
         return (innerPath, outerPath)
     }
     
-    func getInnerSemiCircleBeizerpath(withRadius radius: CGFloat) -> UIBezierPath {
+    func getInnerSemiCircleBeizerpath(withRadius radius: CGFloat) -> (path: UIBezierPath, radius: CGFloat) {
         let center = CGPoint(x: 0, y: bounds.height / 2)
+        let radiusFinal = radius/2 + 10
 
         // Generating Beizerpath for Inner Semi circle
         let innerSemiCirclePath = UIBezierPath(arcCenter: center,
-                                               radius: radius/2 + 10,
+                                               radius: radiusFinal,
                                                startAngle: 0,
                                                endAngle: 180,
                                                clockwise: true)
         // Fill color is always White
         UIColor.white.setFill()
-        return innerSemiCirclePath
+        return (innerSemiCirclePath, radiusFinal)
     }
     
-    func getOuterSemiCircleBeizerPath(withRadius radius: CGFloat) -> UIBezierPath {
+    func getOuterSemiCircleBeizerPath(withRadius radius: CGFloat) -> (path: UIBezierPath, radius: CGFloat) {
         // Drawing Semicircle on the left corner
         let color = UIColor.init(red: 51/255, green: 138/255, blue: 248/255, alpha: 1)
         
@@ -52,11 +149,9 @@ class DiscView: UIView {
         
         // Drawing outer circle
         var radiusFinal = radius * 2
-        print(UIScreen.main.bounds.height/2)
-        print(UIScreen.main.bounds.width - 10)
         if radius == 200 {
-            radiusFinal = UIScreen.main.bounds.height/2
-//            radiusFinal = min(UIScreen.main.bounds.width - 10, UIScreen.main.bounds.height - 20)
+            radiusFinal = min(UIScreen.main.bounds.width - 10, UIScreen.main.bounds.height/2 - 20)
+            print("Radius Final: \(radiusFinal)")
         }
         let outerSemiCirclePath = UIBezierPath(arcCenter: center,
                                                radius: radiusFinal,
@@ -64,20 +159,7 @@ class DiscView: UIView {
                                                endAngle: 180,
                                                clockwise: true)
         color.setFill()
-        return outerSemiCirclePath
-    }
-    
-    var enlargedDiscFrame: CGRect = {
-        let frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 10, height: UIScreen.main.bounds.height)
-        return frame
-    }()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        return (outerSemiCirclePath, radiusFinal)
     }
     
     func paintDisc() {
@@ -87,79 +169,8 @@ class DiscView: UIView {
             draw(self.bounds)
         } else {
             smallDiscModeOn = true
-            //chande to small
             self.frame = smallDiscFrame
             self.draw(self.bounds)
-        }
-    }
-    
-    override func draw(_ rect: CGRect) {
-        if smallDiscModeOn {
-            let path1 = smallDiscPath().outer
-            
-//            var animationsGroup = [CABasicAnimation]()
-//            let center = CGPoint(x: 0, y: bounds.height / 2)
-            let color = UIColor.init(red: 51/255, green: 138/255, blue: 248/255, alpha: 1)
-            let shapeLayer = getCAShapeLayer(forPath: path1, fillColor: color)
-            
-            if fromEnlargedState {
-                let shapeLayer = getCAShapeLayer(forPath: enlargedDiscPath().outer, fillColor: .white)
-                
-                addLayer(shapeLayer: shapeLayer)
-                fromEnlargedState = false
-            }
-            
-            let shapeLayer1 = getCAShapeLayer(forPath: smallDiscPath().inner, fillColor: .white)
-            
-//            let animationGroup = CAAnimationGroup()
-//            animationGroup.animations = animationsGroup
-//            animationGroup.duration = 1.0
-//            animationGroup.autoreverses = false
-//            animationGroup.repeatCount = 0
-//            animationGroup.fillMode = kCAFillModeBackwards
-//            animationGroup.isRemovedOnCompletion = false
-//            shapeLayer.add(animationGroup, forKey: nil)
-            // remove existing shapelayers
-            
-            
-            addLayer(shapeLayer: shapeLayer)
-            addLayer(shapeLayer: shapeLayer1)
-        } else {
-            layer.sublayers = nil
-            let path1 = enlargedDiscPath().outer
-
-            let color = UIColor.init(red: 51/255, green: 138/255, blue: 248/255, alpha: 1)
-            let shapeLayer = getCAShapeLayer(forPath: path1, fillColor: color)
-            
-//            var animationsGroup = [CABasicAnimation]()
-//            let center = CGPoint(x: 0, y: bounds.height / 2)
-            
-//            for i in 100...Int(bounds.width) {
-//                let path = UIBezierPath(arcCenter: center,
-//                                        radius: CGFloat(i),
-//                                        startAngle: 0,
-//                                        endAngle: 180,
-//                                        clockwise: true)
-//
-//                let pathAnim = CABasicAnimation(keyPath: "path")
-//                pathAnim.toValue = path.cgPath
-//                animationsGroup.append(pathAnim)
-//            }
-//
-//            let animationGroup = CAAnimationGroup()
-//            animationGroup.animations = animationsGroup
-//            animationGroup.duration = 1.0
-//            animationGroup.autoreverses = false
-//            animationGroup.repeatCount = 0
-//            animationGroup.fillMode = kCAFillModeForwards
-//            animationGroup.isRemovedOnCompletion = false
-//            shapeLayer.add(animationGroup, forKey: nil)
-            
-            let shapeLayer1 = getCAShapeLayer(forPath: enlargedDiscPath().inner, fillColor: .white)
-
-            addLayer(shapeLayer: shapeLayer)
-            addLayer(shapeLayer: shapeLayer1)
-            fromEnlargedState = true
         }
     }
     
@@ -174,6 +185,59 @@ class DiscView: UIView {
         return shapeLayer
     }
     
+    func getAnimationGroup(withLowerLimit lowerLimit: Int, upperLimit: Int, animsGroup: [CABasicAnimation]? = nil, fillMode: String = kCAFillModeForwards) -> CAAnimationGroup {
+        var animationsGroup = [CABasicAnimation]()
+        let animationGroup = CAAnimationGroup()
+        let animationDuration = 2.0
+        
+        if let animsGroup = animsGroup {
+            animationGroup.animations = animsGroup
+        } else {
+
+            for i in lowerLimit...upperLimit {
+                let path = UIBezierPath(arcCenter: centerOfDisc,
+                                        radius: CGFloat(i),
+                                        startAngle: 0,
+                                        endAngle: 180,
+                                        clockwise: true)
+                
+                let pathAnim = CABasicAnimation(keyPath: "path")
+                pathAnim.toValue = path.cgPath
+                animationsGroup.append(pathAnim)
+            }
+            animationGroup.animations = animationsGroup
+        }
+        
+        animationGroup.duration = animationDuration
+        animationGroup.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        animationGroup.autoreverses = false
+        animationGroup.repeatCount = 0
+        animationGroup.fillMode = fillMode
+        animationGroup.isRemovedOnCompletion = false
+        return animationGroup
+    }
+    
+    func makeAnimationsGroupForEnlargedToSmall(withRange range: Int, intialRadius: CGFloat) -> [CABasicAnimation] {
+        var animationsGroup = [CABasicAnimation]()
+        var rangeCount = range
+        var radius = intialRadius
+
+        while rangeCount > 0 {
+            let semPath = UIBezierPath(arcCenter: centerOfDisc,
+                                       radius: radius,
+                                       startAngle: 0,
+                                       endAngle: 180,
+                                       clockwise: true)
+            let pathAnim = CABasicAnimation(keyPath: "path")
+            pathAnim.toValue = semPath.cgPath
+            animationsGroup.append(pathAnim)
+            rangeCount = rangeCount - 1
+            radius = radius - 1.0
+        }
+
+        return animationsGroup
+    }
+
     func addLayer(shapeLayer: CAShapeLayer) {
         if let count = layer.sublayers?.count {
             print("Number of layers found: \(count)")
@@ -183,6 +247,44 @@ class DiscView: UIView {
         }
         layer.addSublayer(shapeLayer)
     }
+
+}
+
+extension UIColor {
     
+    class func partyTimeBlue() -> UIColor {
+        let color = UIColor.init(red: 51/255, green: 138/255, blue: 248/255, alpha: 1)
+        return color
+    }
     
+}
+
+extension UIView {
+    func mask(withRect rect: CGRect, inverse: Bool = false) {
+        let path = UIBezierPath(rect: rect)
+        let maskLayer = CAShapeLayer()
+        
+        if inverse {
+            path.append(UIBezierPath(rect: self.bounds))
+            maskLayer.fillRule = kCAFillRuleEvenOdd
+        }
+        
+        maskLayer.path = path.cgPath
+        
+        self.layer.mask = maskLayer
+    }
+    
+    func mask(withPath path: UIBezierPath, inverse: Bool = false) {
+        let path = path
+        let maskLayer = CAShapeLayer()
+        
+        if inverse {
+            path.append(UIBezierPath(rect: self.bounds))
+            maskLayer.fillRule = kCAFillRuleEvenOdd
+        }
+        
+        maskLayer.path = path.cgPath
+        
+        self.layer.mask = maskLayer
+    }
 }
--- PartyTime/ViewController.swift
+++ PartyTime/ViewController.swift
@@ -14,14 +14,20 @@ class ViewController: UIViewController {
     var discView: DiscView = {
         let frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width / 2 + 20, height: UIScreen.main.bounds.height - 10)
         let discView = DiscView(frame: frame)
-        discView.clipsToBounds = true
+        discView.clipsToBounds = false
         discView.translatesAutoresizingMaskIntoConstraints = false
-        discView.backgroundColor = .white
-       return discView
+        discView.backgroundColor = .lightGray
+//        discView.layer.cornerRadius = frame.width / 2
+//        discView.layer.masksToBounds = true
+        return discView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func loadView() {
+        super.loadView()
         createDiscView()
     }
     
@@ -29,8 +35,6 @@ class ViewController: UIViewController {
         self.view.addSubview(discView)
         discView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
         discView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
-//        discView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 10).isActive = true
-//        discView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
         discView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height - 20).isActive = true
         discView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width / 2 + 20).isActive = true
         
