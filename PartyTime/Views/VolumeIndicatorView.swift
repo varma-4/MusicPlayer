c55ec3d228c608ca37d5a5b2ec394f0c0d7f8041
varma-4 <varmadatla444@gmail.com>
2018-05-09 12:18:06 +0530

    Added volume up and volume down Assets to project & respective image views to VolumeIndicator View

--- PartyTime/Views/VolumeIndicatorView.swift
+++ PartyTime/Views/VolumeIndicatorView.swift
@@ -15,10 +15,46 @@ class VolumeIndicatorView: IndicatorView {
         backgroundColor = .clear
         configureShapeLayers()
         configureGesture()
+        addVolumeIcons()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+    
+    var volumeDownView: UIImageView = {
+        let imageFrame = CGRect(x: 0, y: 0, width: 40, height: 40)
+        let iv = UIImageView(frame: imageFrame)
+        iv.contentMode = UIViewContentMode.scaleAspectFill
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return UIImageView()
+    }()
+    
+    var volumeUpView: UIImageView = {
+        let imageFrame = CGRect(x: 0, y: 0, width: 40, height: 40)
+        let iv = UIImageView(frame: imageFrame)
+        iv.contentMode = UIViewContentMode.scaleAspectFill
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return UIImageView()
+    }()
+    
+    func addVolumeIcons() {
+        addSubview(volumeDownView)
+        addSubview(volumeUpView)
+        
+        volumeDownView.image = #imageLiteral(resourceName: "volumeDown").withRenderingMode(.alwaysOriginal)
+        volumeDownView.translatesAutoresizingMaskIntoConstraints = false
+        volumeDownView.rightAnchor.constraint(equalTo: self.centerXAnchor, constant: 30).isActive = true
+        volumeDownView.topAnchor.constraint(equalTo: self.topAnchor, constant: 42).isActive = true
+        volumeDownView.widthAnchor.constraint(equalToConstant: 20).isActive = true
+        volumeDownView.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        
+        volumeUpView.image = #imageLiteral(resourceName: "volumeUp").withRenderingMode(.alwaysOriginal)
+        volumeUpView.translatesAutoresizingMaskIntoConstraints = false
+        volumeUpView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: 10).isActive = true
+        volumeUpView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -12).isActive = true
+        volumeUpView.widthAnchor.constraint(equalToConstant: 20).isActive = true
+        volumeUpView.heightAnchor.constraint(equalToConstant: 20).isActive = true
     }
     
 }
